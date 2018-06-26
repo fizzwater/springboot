3189fd37e89e2bef71b5803ebc8551bb871e7c9a
qinchao <craig.qin@yoho01.local>
2018-06-26 17:13:44 +0800

    打war包的时候，添加listener

--- src/main/java/com/qin/springboot/Application.java
+++ src/main/java/com/qin/springboot/Application.java
@@ -35,7 +35,7 @@ public class Application  extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+        return application.listeners(new LoadAdditionalPropertiesListener()).sources(Application.class);
     }
 
     public static void main( String[] args ) {
